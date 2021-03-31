64abb2238b3e1a276c9e68e01d8163b02f935613
Muhsin Neyyathur <muhsinny333@gmail.com>
2021-03-31 13:22:15 +0530

    share code on github

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,14 @@ const app = express();
 app.get('/api/products',(req,res) => {
     res.send(data.products)
 })
+app.get('/api/products/:id',(req,res) => {
+    const product = data.products.find(x => x._id === req.params.id)
+    if(product){
+        res.send(product);
+    }else{
+        res.status(404).send({message:'product not found'})
+    }
+})
 app.get('/',(req,res) => {
     res.send('server is ready')
 })
